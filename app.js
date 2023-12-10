c317aa2ab1100df1315d8fcf43402744dde4d5e6
Yannick <girofare@hotmail.com>
2023-12-10 12:11:14 +0100

    implemented ejs

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express')
+const {status} = require("express/lib/response")
 const app = express()
 
+//implémentation du moteur d'affichage ejs
+app.set("view engine", "ejs");
+    //définition de l'endroit où se trouvent les views
+app.set("views", "components");
+
 app.get('/', (req, res)=>{
-    res.status(300).redirect('/home')
+    res.status(300).render('home')
 })
 
 app.get('/home', (req, res)=> {
-res.status(200).sendFile('./components/Home.html', {root: __dirname})
+res.status(200).render('Home')
 })
 
 app.get('/apropos', (req, res)=> {
-    res.status(200).sendFile('./components/Apropos.html', {root: __dirname})
+    res.status(200).render('Apropos')
     })
 
 app.use((req, res)=> {
-        res.status(404).sendFile('./components/PageInstrouvable.html', {root: __dirname})
+        res.status(404).render('PageIntrouvable')
         })
 
 
