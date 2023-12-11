a1f8c5edf1ffab71a6526b78c00525ac9fdc84ea
Yannick <girofare@hotmail.com>
2023-12-11 16:37:29 +0100

    added connection to mysqlbdd and tried it

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const {status} = require("express/lib/response")
 const app = express()
+const mysql = require('mysql2')
+const myConnection = require('express-myconnection')
+
+//création d'un objet contenant les différents paramètres de connexion à la bdd mysql
+const optionsbd = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'notes_bd'}
+
+ //middleware pour connexion, va notamment ajouter à req la méthode getConnection()
+ app.use(myConnection(mysql, optionsbd,'pool'))   
 
 //implémentation du moteur d'affichage ejs
 app.set("view engine", "ejs");
@@ -19,7 +32,18 @@ app.get('/home', (req, res)=> {
         {id : 2, todo: "Shopping", date: 'today'},
         {id : 3, todo: "Clean up house", date: 'after tomorrow'},
     ]
-res.status(200).render('Home', {name,datas,titlePage} )
+    req.getConnection((erreur, connection)=>{
+        if(erreur)
+        console.log(erreur);
+        else 
+        connection.query('SELECT * FROM notes', [], (erreur, resultat) => {
+        if (erreur)
+            console.log(erreur);
+        else 
+            res.status(200).render('Home', {resultat, name,datas,titlePage})
+        } )
+    })
+    
 })
 
 app.get('/apropos', (req, res)=> {
